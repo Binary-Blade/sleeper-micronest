3aeb01760005aa1cd4fc130a36d973eab08c46f3
Binary-Blade <codegrunt@proton.me>
2024-03-18 04:05:59 +0100

    * feat: Add pino-http and pino-pretty for better logger result of response and request to create a better view of the request/response logic

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
+import { DatabaseModule } from '@app/common';
+import { ReservationRepository } from './reservations.repository';
+import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
+import { LoggerModule } from 'nestjs-pino';
+
+@Module({
+  imports: [DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema }
+    ]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty'
+        }
+      }
+    })],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationRepository],
+})
+export class ReservationsModule { }
