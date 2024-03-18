da946159afdadceb8d354fe76a4bc0013cd3338b
Binary-Blade <codegrunt@proton.me>
2024-03-19 00:44:13 +0100

    * fix: add <string> type for URI mongodb

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '../config/config.module';
 
 @Module({
 	imports: [MongooseModule.forRootAsync({
-		imports: [ConfigModule],
 		useFactory: (configService: ConfigService) => ({
-			uri: configService.get('MONGODB_URI'),
+			uri: configService.get<string>('MONGODB_URI'),
 		}),
 		inject: [ConfigService],
 	})]
