6ff64cede32c0a3847cd807b15e75ccc7b4fc642
Binary-Blade <codegrunt@proton.me>
2024-03-18 04:24:37 +0100

    * feat: Add class validator and class transform for the date type

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,23 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+
+  @IsNotEmpty()
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
+
+  @IsNotEmpty()
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
