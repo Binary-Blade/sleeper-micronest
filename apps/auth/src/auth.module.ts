ab95df3ca8173098d62eb755d615b16ac508dd24
Binary-Blade <codegrunt@proton.me>
2024-03-19 23:16:19 +0100

    * feat: Add JWT strategy and authentification logic for auth microservice

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -7,6 +7,7 @@ import { LoggerModule } from '@app/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LocalStrategy } from './strategies/local.strategy';
 import * as Joi from 'joi';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -32,6 +33,6 @@ import * as Joi from 'joi';
       inject: [ConfigService]
     })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule { }
